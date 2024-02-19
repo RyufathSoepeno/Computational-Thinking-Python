9beb9a92eceaa7a74a2f9f7128732517ef0d9a64
AllisterTotong <92731771+AllisterTotong@users.noreply.github.com>
2024-02-19 10:09:04 +0700

    Update Week5.py

--- Week5.py
+++ Week5.py
@@ -1,4 +1,4 @@
-
+# Question 1
 
 # Question 2
 
@@ -12,3 +12,20 @@ else:
   ab.lower()
 
 print(ab)
+
+#Question 3
+
+def checkGPA(GPA): #function to check if accepted or not
+    instring = GPA
+    if GPA > 2: #if above 2.0, accepted
+        print("Your application is accepted, welcome to the university")
+    else: #if else 2.0, rejected
+        print("Your application is denied, sorry")
+
+print("number 3")
+inGPA = float(input("input your GPA:")) #input GPA score, use float to allow decimal number in GPA
+while inGPA < 0 or inGPA > 4: #to ensure that input is between 0 and 4
+    inGPA = float(input("wrong format, input your GPA:"))
+checkGPA(inGPA)#call function to check if accepted or not
+
+# Question 4
