08f0f22a1ed5136e342007246c66258faef297a3
Ryufath Alief Adhyaksa Putera Soepeno <89006072+ryu0210@users.noreply.github.com>
2024-02-19 09:57:36 +0700

    Update Week5.py

--- Week5.py
+++ Week5.py
@@ -1,1 +1,14 @@
 
+
+# Question 2
+
+z = char(input("Press S to convert to capital letter; Press D to convert to lower letter"))
+
+if z == s:
+  ab = input("Input your letter")
+  ab.upper()
+else:
+  ab = input("Input your letter")
+  ab.lower()
+
+print(ab)
