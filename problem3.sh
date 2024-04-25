66ba06f76116d9afc277025ef5666c79d221c113
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-04-25 17:12:20 +0700

    Add files via upload

---
+++ problem3.sh
@@ -0,0 +1,13 @@
+# Using ps to list proccesses, we can  pipe the output to wc to count lines
+
+num_processes=$(ps -u $(whoami) | wc -1)
+
+
+# Then we use pre-incrementation operation to excluse any header line from the count
+
+num_processes=$((num_processes - 1))
+
+
+# Now, we print it
+
+echo "Number of current processes for your account: $num_processes"
